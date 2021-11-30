78e1765ff1a4aa7d0af4c211512df09df6ca3a29
Vyrago <52431894+Vyrago@users.noreply.github.com>
2021-11-29 18:05:11 -0800

    Update Lion.java

--- animal/carnivore/Lion.java
+++ animal/carnivore/Lion.java
@@ -3,7 +3,6 @@ import animal.Animal;
 
 public class Lion implements Animal {
 
-//3. In the Giraffe and Lion classes, create private members variables called "name" and "type". ->
 	private String name;
 	private String type = "Lion";
 
@@ -11,13 +10,11 @@ public class Lion implements Animal {
 		this.name = name;
 	}
 
-//-> Modify getName to return the this.name variable. ->
 	@Override
 	public String getName() {
 		return this.name;
 	}
 
-//-> Implement the getType() function to return the this.type variable.
 	@Override
 	public String getType() {
 		return this.type;
